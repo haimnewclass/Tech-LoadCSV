f4efa32128f66046ba7862a2890ee1d59cc04c16
haim hod gilboa <haim@newclass.co.il>
2022-03-24 11:48:15 +0200

    Add project files.

---
+++ CSVProcess/Mng.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVProcess
+{
+    public class Mng
+    {
+        public void Run()
+        {
+            
+            string fileName = @"C:\Users\haim\source\repos\Tech-CSV\CSVProcess\Data\Lotto.csv";
+            PaisFile pais = new PaisFile();
+            pais.Load(fileName);
+        }
+    }
+}
---
+++ CSVProcess/PaisFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CSVProcess
+{
+    public class PaisFile
+    {
+        public void Load(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            for(int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] values = line.Split(',');
+
+
+            }
+
+        }
+    }
+}
